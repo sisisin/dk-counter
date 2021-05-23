eb76b44e11b546eb41d4194f774ea52bc7d70626
sisisin <azsisisin@gmail.com>
2021-05-23 19:19:25 +0900

    setup vite

--- app/src/renderer/preload.js
+++ app/src/renderer/preload.js
@@ -1,26 +1,10 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
-const { ipcRenderer } = require('electron');
+const { ipcRenderer, contextBridge } = require('electron');
 
-window.addEventListener('DOMContentLoaded', () => {
-  document.querySelector('#scoredb').addEventListener('change', (e) => {
-    ipcRenderer.send('foo', e.target.files[0].path);
-    e.target.value = '';
-  });
-
-  document.querySelector('#authorize-twitter-anchor').addEventListener('click', (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-    ipcRenderer.send('clickTwitterAnchor');
-  });
-
-  document.querySelector('#pin-submit').addEventListener('click', () => {
-    const pinInput = document.querySelector('#twitter-pin');
-    const pin = pinInput.value;
-    ipcRenderer.send('authorizeTwitter', pin);
-  });
-
-  document.querySelector('#tweet-daken-count').addEventListener('click', () => {
-    ipcRenderer.send('tweet');
-  });
+contextBridge.exposeInMainWorld('appEvents', {
+  sendTwitterAnchorClicked: () => ipcRenderer.send('clickTwitterAnchor'),
+  sendAuthorizeTwitterClicked: (pin) => ipcRenderer.send('authorizeTwitter', pin),
+  sendScoreDBPath: (path) => ipcRenderer.send('foo', path),
+  sendTweetClicked: () => ipcRenderer.send('tweet'),
 });
--- app/src/renderer/renderer.js
+++ app/src/renderer/renderer.js
@@ -1,6 +1,22 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// No Node.js APIs are available in this process because
-// `nodeIntegration` is turned off. Use `preload.js` to
-// selectively enable features needed in the rendering
-// process.
+window.addEventListener('DOMContentLoaded', () => {
+  document.querySelector('#scoredb').addEventListener('change', (e) => {
+    appEvents.sendScoreDBPath(e.target.files[0].path);
+    e.target.value = '';
+  });
+
+  document.querySelector('#authorize-twitter-anchor').addEventListener('click', (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    appEvents.sendTwitterAnchorClicked();
+  });
+
+  document.querySelector('#pin-submit').addEventListener('click', () => {
+    const pinInput = document.querySelector('#twitter-pin');
+    const pin = pinInput.value;
+    appEvents.sendAuthorizeTwitterClicked(pin);
+  });
+
+  document.querySelector('#tweet-daken-count').addEventListener('click', () => {
+    appEvents.sendTweetClicked();
+  });
+});
