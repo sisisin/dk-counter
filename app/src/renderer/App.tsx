a32761635c900d11efa0b89199fbc64e609807a7
sisisin <azsisisin@gmail.com>
2021-05-23 21:19:11 +0900

    move to react

--- app/src/renderer/App.tsx
+++ app/src/renderer/App.tsx
@@ -1,5 +1,46 @@
 import React from 'react';
 
 export const App: React.FC = () => {
-  return <div>hello</div>;
+  const pinInputRef = React.useRef<HTMLInputElement>(null);
+  const scoreDBInputRef = React.useRef<HTMLInputElement>(null);
+
+  const handlePinSubmit = (e) => {
+    const pin = pinInputRef.current.value;
+    appEvents.sendAuthorizeTwitterClicked(pin);
+  };
+
+  const handleScoreDBSet = () => {
+    const path = (scoreDBInputRef.current.files[0] as any).path;
+
+    appEvents.sendScoreDBPath(path);
+  };
+  return (
+    <div>
+      <div>
+        <div>
+          <a
+            href="#"
+            onClick={(e) => {
+              e.preventDefault();
+              e.stopPropagation();
+              appEvents.sendTwitterAnchorClicked();
+            }}
+          >
+            authorize with twitter
+          </a>
+        </div>
+        <div>twitter pin</div>
+        <input name="twitter-pin" ref={pinInputRef} />
+        <button id="pin-submit" onClick={handlePinSubmit}>
+          submit
+        </button>
+      </div>
+      {/* <!-- todo: https://developer.mozilla.org/ja/docs/Web/API/File/Using_files_from_web_applications --> */}
+      <input name="scoredb" type="file" ref={scoreDBInputRef} onChange={handleScoreDBSet} />
+
+      <div>
+        <button onClick={() => appEvents.sendTweetClicked()}>tweet</button>
+      </div>
+    </div>
+  );
 };
--- app/src/renderer/renderer.tsx
+++ app/src/renderer/renderer.tsx
@@ -8,28 +8,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root'),
 );
-
-declare const appEvents;
-
-window.addEventListener('DOMContentLoaded', () => {
-  document.querySelector('#scoredb').addEventListener('change', (e: any) => {
-    appEvents.sendScoreDBPath(e.target.files[0].path);
-    e.target.value = '';
-  });
-
-  document.querySelector('#authorize-twitter-anchor').addEventListener('click', (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-    appEvents.sendTwitterAnchorClicked();
-  });
-
-  document.querySelector('#pin-submit').addEventListener('click', () => {
-    const pin = (document.querySelector('#twitter-pin') as HTMLInputElement).value;
-
-    appEvents.sendAuthorizeTwitterClicked(pin);
-  });
-
-  document.querySelector('#tweet-daken-count').addEventListener('click', () => {
-    appEvents.sendTweetClicked();
-  });
-});
